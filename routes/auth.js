77d57c1f4a2e59796f750079c4187994aae8d4c0
GerardoRosas <rosas.ruizlg@gmail.com>
2020-11-17 23:14:49 -0600

    Autenticacion User

--- routes/auth.js
+++ routes/auth.js
@@ -3,16 +3,19 @@ const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
 const authController = require('../controllers/authController');
+const auth = require('../middleware/auth');
 
+//Iniciar sesion
 // api/auth
 
 router.post('/', 
-    [
-        check('email', 'Agrega un email válido').not().isEmpty(),
-        check('password', 'El password debe ser mínimo 6 caracteres').isLength({min: 6})
-    ],
     authController.autenticarUsuario
-
 );
+
+//Obtiene el usuario autenticado
+router.get('/',
+    auth,
+    authController.usuarioAutenticado
+)
 
 module.exports = router;
